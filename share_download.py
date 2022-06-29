dc4ef7f9849c88c65b81b191beb4387cb7717156
Milan Capoor <capoorm123@gmail.com>
2022-06-29 11:59:44 -0400

    commit dist project

--- share_download.py
+++ share_download.py
@@ -1,7 +1,6 @@
 import pyperclip
 import time
 import pyautogui
-import click
 import PIL.ImageGrab
 import os
 
@@ -16,8 +15,9 @@ UPLOAD_LOCATION = (6 * PPI, 0.75 * PPI)
 YELLOW_LOCATION = (10 * PPI, 4.5 * PPI)
 GREY_BACK_LOCATION = (3 * PPI, 2.5 * PPI)
 
-f = open('colors.txt', 'r')
-SHARE_NOT_TOGGLED_COLOR, YELLOW_COLOR, GREY_COLOR = f.readlines()
+SHARE_NOT_TOGGLED_COLOR = (228, 231, 233)
+YELLOW_COLOR = (247, 245, 226)
+GREY_COLOR = (123, 123, 123)
 
 files = os.listdir()
 
@@ -29,19 +29,19 @@ for file in files:
     while (get_color(GREY_BACK_LOCATION) == GREY_COLOR):
         time.sleep(1)
 
-    click.click(CONTROL_LOCATION) 
+    pyautogui.click(CONTROL_LOCATION) 
     if (get_color(SHARE_LOCATION) == SHARE_NOT_TOGGLED_COLOR):
-        click.click(SHARE_LOCATION)
-        click.clicK((SHARE_LOCATION[0], SHARE_LOCATION[1] + 50))
-        click.click(UPLOAD_LOCATION)
+        pyautogui.click(SHARE_LOCATION)
+        pyautogui.click((SHARE_LOCATION[0], SHARE_LOCATION[1] + 50))
+        pyautogui.click(UPLOAD_LOCATION)
         pyautogui.hotkey('enter')
        
         while (get_color(YELLOW_LOCATION) != YELLOW_COLOR):
             time.sleep(1) 
             
         pyautogui.hotkey('esc')
-        click.click(HOME_LOCATION)
+        pyautogui.click(HOME_LOCATION)
         continue
     else: 
-        click.click(HOME_LOCATION)
+        pyautogui.click(HOME_LOCATION)
         continue
