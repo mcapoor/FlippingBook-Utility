70563b9a123f9613663a266d99d8c371409a720a
Milan Capoor <capoorm123@gmail.com>
2022-06-29 13:49:54 -0400

    new precision

--- share_download.py
+++ share_download.py
@@ -8,16 +8,17 @@ def get_color(location):
     return PIL.ImageGrab.grab().load()[location]
 
 PPI = 100.7
-CONTROL_LOCATION = (1 * PPI, 8 * PPI)
-SHARE_LOCATION = (1.5 * PPI, 5.75 * PPI)
-HOME_LOCATION = (0.5 * PPI, 0.75 * PPI)
-UPLOAD_LOCATION = (6 * PPI, 0.75 * PPI)
-YELLOW_LOCATION = (10 * PPI, 4.5 * PPI)
-GREY_BACK_LOCATION = (2 * PPI, 3 * PPI)
+CONTROL_LOCATION = (175, 750)
+SHARE_LOCATION = (138, 542)
+PRINT_LOCATION = (138, 585)
+HOME_LOCATION = (50, 67)
+UPLOAD_LOCATION = (560, 65)
+YELLOW_LOCATION = (1205, 411)
+GREY_BACK_LOCATION = (250, 300)
 
 SHARE_NOT_TOGGLED_COLOR = (228, 231, 233)
 YELLOW_COLOR = (247, 245, 226)
-GREY_COLOR = (123, 123, 123)
+GREY_COLOR = (124, 124, 124)
 
 with open("target_dir.txt", 'r') as f:
     path = f.read().strip("'")
@@ -32,6 +33,7 @@ for file in files:
     pyautogui.hotkey('ctrl', 'o', interval = 0.15)
     time.sleep(2)
     pyperclip.paste()
+    time.sleep(2)
     pyautogui.hotkey('enter')
     
     while (get_color(GREY_BACK_LOCATION) == GREY_COLOR):
@@ -40,10 +42,10 @@ for file in files:
 
     pyautogui.click(CONTROL_LOCATION) 
     
-    if (get_color(SHARE_LOCATION) == SHARE_NOT_TOGGLED_COLOR):
+    if (get_color(SHARE_LOCATION) != SHARE_NOT_TOGGLED_COLOR):
         pyautogui.click(SHARE_LOCATION)
         time.sleep(0.3)
-        pyautogui.click((SHARE_LOCATION[0], SHARE_LOCATION[1] + 50))
+        pyautogui.click(PRINT_LOCATION)
         time.sleep(0.3)
         pyautogui.click(UPLOAD_LOCATION)
         time.sleep(0.3)
@@ -51,7 +53,7 @@ for file in files:
        
         while (get_color(YELLOW_LOCATION) != YELLOW_COLOR):
             print("waiting to upload")
-            time.sleep(1) 
+            time.sleep(3) 
             
         pyautogui.hotkey('esc')
         time.sleep(0.3)
